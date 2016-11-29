5f2e80c908c0c52ac4fdcd34377c79d95a69e725
Maria Paulina <je.mapau@gmail.com>
2016-11-28 19:59:34 -0500

    Server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hola mundo!');
+  res.send('Hola mundo');
 })
 
 app.listen(3000, function (err) {
