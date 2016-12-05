521b015638110d000972ed0d4392d51df91f33e7
Maria Paulina <je.mapau@gmail.com>
2016-12-04 22:21:45 -0500

    Setting routes with pages

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index');
+})
+
+app.get('/signup', function (req, res) {
+  res.render('index');
+})
+
+app.get('/signin', function (req, res) {
+  res.render('index');
 })
 
 app.listen(3000, function (err) {
